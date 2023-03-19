244d476e00b5aa8042ce3314c92b58a778794ab2
Alexander <alex.olsh85@gmail.com>
2023-03-19 20:21:30 +0200

    cleanup

--- verySimpleApp/Program.cs
+++ verySimpleApp/Program.cs
@@ -9,28 +9,22 @@ namespace HttpListenerExample
     {
         static async Task Main(string[] args)
         {
-            // Create an HttpListener instance with a prefix
             using (HttpListener listener = new HttpListener())
             {
                 listener.Prefixes.Add("http://localhost:8080/");
 
-                // Start listening for incoming requests
                 listener.Start();
                 Console.WriteLine("Listening...");
 
-                // Loop until stopped
                 bool running = true;
                 while (running)
                 {
                     try
                     {
-                        // Get the context of the request
                         HttpListenerContext context = await listener.GetContextAsync();
 
-                        // Check if it is a GET request
                         if (context.Request.HttpMethod == "GET")
                         {
-                            // Write some response to the output stream
                             byte[] buffer = Encoding.UTF8.GetBytes("The current host name is: " + Dns.GetHostName());
                             context.Response.ContentLength64 = buffer.Length;
                             using (var stream = context.Response.OutputStream)
@@ -38,19 +32,16 @@ namespace HttpListenerExample
                                 stream.Write(buffer, 0, buffer.Length);
                             }
 
-                            // Close the output stream and send the response
                             context.Response.Close();
                         }
                     }
                     catch (Exception e)
                     {
-                        // Handle any exceptions that may occur
                         Console.WriteLine("Error: " + e.Message);
-                        running = false; // Stop listening on error
+                        running = false;
                     }
                 }
 
-                // Stop listening when done
                 listener.Stop();
             }
         }
